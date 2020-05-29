1e0d790acc371ebc06d490de4478c6f8f4b40b94
Camilo Mora <camilo.mora@vasslatam.com>
2020-05-29 15:55:14 -0500

    add hooks in chat component, call socket io library

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString  from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [ name, setName ] = useState('');
+    const [ room, setRoom ] = useState('');
+    const ENDPOINT = 'localhost:5000';
+    useEffect(() => {
+        // Retrieve data from queryString
+        const { name, room } = queryString.parse(location.search);
+        socket = io(ENDPOINT);
+
+        console.log(socket);
+        setName(name);
+        setName(room);
+        socket.emit('join',{ name, room });
+
+    }, [ENDPOINT, location.search]);
+
     return(
         <h1>Chat</h1>
     )
