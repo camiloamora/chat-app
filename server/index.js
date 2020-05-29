952dae93f295456e441b6d7dac969145fded17f7
Camilo Mora <camilo.mora@vasslatam.com>
2020-05-29 15:11:43 -0500

    add router server, add socket server connection

--- server/index.js
+++ server/index.js
@@ -3,9 +3,18 @@ const socketio = require('socket.io');
 const http = require('http');
 
 const PORT = process.env.PORT || 5000;
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('We have a new connection!°!!!!');
+    socket.on('disconnect', () => {
+        console.log('user had left!!!!!');
+    })
+});
+
+app.use(router);
 server.listen(PORT, () => console.log(`Server has started in port ${PORT}`));
